63752621da91b434d5f0f292fe5afb0ace518b95
zac234 <zacbenincasa@gamil.com>
2022-02-22 19:22:56 -0700

    hopefully that works right

---
+++ app.js
@@ -0,0 +1,34 @@
+var userage = 17
+var is_subscribed = true
+var not_subscribed = true
+
+
+
+
+if (userage >= 18, is_subscribed) {
+    console.log("user is old enough") 
+} else{
+    console.log("user is not old enough")
+}
+
+if (userage <= 18, is_subscribed){
+    console.log("you are to young to be here")
+}else{
+    console.log("go right on in")
+}
+
+if (userage >= 18, not_subscribed){
+    console.log ("make an account next time")
+} else if(is_subscribed){
+    console.log ("welcome")
+}else{
+    console.log ("try again next time")
+}
+
+if (userage <=18,not_subscribed){
+    console.log ("you are too young and not subcribed")
+} else if(is_subscribed){
+    console.log ("you are too young to be here")
+}else{
+    console.log("maybe nect time")
+}
